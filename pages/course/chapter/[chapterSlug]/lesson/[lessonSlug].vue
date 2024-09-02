668b86ad395a8a3847dbcf66ccf25b25a00417a3
ahmadalanib <alnaib888@gmail.com>
2024-09-02 21:11:06 +0200

    add data

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,25 @@
+<script setup>
+const course=useCourse()
+const route=useRoute()
+
+const chapter=computed(()=>{
+  return course.chapters.find(
+    (chapter)=>chapter.slug===route.params.chapterSlug
+  )
+})
+
+const lesson=computed(()=>{
+  return chapter.value.lessons.find(
+    (lesson)=>lesson.slug===route.params.lessonSlug
+  )
+})
+
+</script>
+
+
+<template>
+  <div>
+    <h1>{{chapter.title}}</h1>
+    <p>{{lesson.title}}</p>
+  </div>
+</template>
